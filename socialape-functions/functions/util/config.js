be2477b9f9beef50a958df34edddeb5ae307e0c3
colinosu <osullivancj@gmail.com>
2020-05-15 17:56:52 -0400

    Added user image upload and default profile image

--- socialape-functions/functions/util/config.js
+++ socialape-functions/functions/util/config.js
@@ -1,17 +1,17 @@
-module.exports = {
-  "type": "service_account",
-  "project_id": "socialape-4e17c",
-  "private_key_id": "c9269a2b7a57d0c629995a174e1d4d5a4d9d79cb",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCl+oGLzhebayN1\nJJjjLu2dmtiTg4DP4t2BnUNvM2AL5cjYRco119cJ4GYt87IfNTWjy2w6gE1onsoI\n1B7ok54io6dmEVRze2gl9RQcBL5jIjDKx0/lETraMRYnI7lvCPJu+O/RBpDHP0zU\nctARJCaoIQxnZYnUVciQYKwogTlGM0yUE9jgxAhyVUgr+LZiRtnFpf0HNNGZ4Cjq\nQrU7ACTwDddtR0oeUwzZe2O/OjweNVkZNNMV/BpcjXFS7gsy6Ysmy+ujaaYWiD2u\nmp6APPBJtVo7n0P7O16Qn9opFznfcsTXSeW+amg96cC+Ky9Z/7/XpaJ4GrtkzixG\n5q/SBBd1AgMBAAECggEAIiAbs4VnmCZSj3q7TnZvF2jnX4j5moRv+368hyf+duPD\nRCgxN5nNljPDfm4s8LqtcSRZd7u6bqi7LE6Enlf/euq/973Xabj1RFybylj1VgeJ\nFaAeHqBIw+S0w85WCWzC/6sCD610ZervXnzOp7+RjR7iYH3hdT0/8+NUci2De5mq\nv2kxNOeXOcevLXE39RjINS4dhlxHSnK6o+VAv7BIMotaVvWlDh/SVPnjcaK0FRwO\nekz684/KC5TC5GW0s+KJWysxJx1o9E/LFyACKBRp4r0A7IjY6qQ2Ykjn40bp2p2j\nvh6bHU9CVdYQwiCnhh1SITK8S5BCIuGnqpTF7W4RbwKBgQDQs2yVqEvUiXfMaqTV\nGhrLE96vLlyGKiolwDErDAbzWENaDmIdJCveSJzlNRVGOn9WnWAU/HK42dRHeVBt\nnytJ2oigV2PKJYPIR3aez4Bio2AF5EvTvjVNMsU8YcWvxWH1SSC5geoM2J2cwXE6\n8tVapWXCqXYCZgC7f3LteeHzOwKBgQDLmGIE37+u97boNSzUbFdjU1avPFEudXUv\nxXFxd5OxzOuY8QtOXM6SdMjdmZQBMBfckwA8U8rDbuRlV4nHYeK6cK7IlDx/nzG0\ny0qKx37uY5U9DqtPHTUqXoEBRae3O6ffkQEIhSVZip9fa+Prl4e2h64ug4Ve6Nzg\nTq/oy0kVDwKBgQCPloLkzK+vgQ0dZVoXDCjXA8q4OGuMG0J4Sq0JcvLtRhLAyRiT\n+Cj+fHMF9+m1MdYen5RVnALKT4SP0FWXtmUwINzThIyxrfHnP9PLG/S26LTOGO5a\ntokxnfa5voMmleT5v+B2AX4obHi0OxtOdXh5L0a+EZ2RtOBYIC3QWYUttQKBgERD\nhaoIs6ag3C0jlzJqWSquiMQcv0OKko4/73eSFm1pPh17xMSzjXfBvI/ArMVxMK7+\ngve3SKcaiW4JWeMGSRUmLxSFz3sjD3GFMCs0FLUtop5kSsAuFONt6GdayY9Aa1sk\nrWoD6rAJMMK31CXsGs7y0045PwDBpz12ZLjzYD+1AoGAZtqGhDBTMDKuz6FGK4p3\nl2fw8iyC5wUyfaB5KY5JFgN4/K9rYcw6DscS29VOP69mRFWmASabbleBrPGBkmOo\nf7hB5SfmJ1d6Tr619eHfX5A1v0rDLzx/cQ+K+fmJQ/Dyy7kqyjvK0xRdQ0k5iXdF\nXFnz8dGuON7M5ZWw3bxW3Go=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-fgrrr@socialape-4e17c.iam.gserviceaccount.com",
-  "client_id": "118420894873735894282",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fgrrr%40socialape-4e17c.iam.gserviceaccount.com"
-}
+// module.exports = {
+//   "type": "service_account",
+//   "project_id": "socialape-4e17c",
+//   "private_key_id": "a95409eb285ab1ed150ac350e018cdcad880a3da",
+//   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQC+0ox0acxxCyvF\nKIbxgbKylh3UOJDi+PtZT8FJNKkWrKwmgSlFl9w8cxUFdukdpFy2di2xx6uar4d1\nhbOomkhLHOvWPqw+F0UtMLedGxAYa7yJ/pKxg88e56I8YiHpDnT+0pIf6Ll68Ef9\njJpaSm639Igpue3QH9TU1qiUujGxonhkihqSQPVWjeBKXA4qdtlxaOFL3skWZCFt\ntjWg/OILZW8xhbdvImcvTIz4Tq2YlHjCPEeB3o8jqq2T4Sj/ki3KMxqw+0ptp+kV\nLuDecGLt3s9ZKdunyuN2N22wm7mqr8+H8UijLOmCdnAbRUrb0wY7BeScbTnwuUdC\ntZ3d690lAgMBAAECggEAVBDd38tcTdDmD42zJWJGH3cS3ROugM+/q7D/LgkNFPgt\nPylXzVkBpX/tmuKwAGcQkTGvH8sfRp5dxuCILZcKkkzDeTdTGmeWCjqUKZpqlNeh\n0AlfBuVSsgLJ7mAtIEdGM50A0x3r2MVScdknhu+Tp2cLNZa7thzP3pRS4HLGwcJW\n1xpMF+8F4yiv29CMFwa9+HWO1usj0LwfrkW2XQo1DXKFz6SpoBWu5MbF8IvOfJPg\nUTW8KaYaAsK0531ErfPIPiExuEEIr6m8oDpx1F/VP6I+8lENj6MQr5DTq2CExNy7\nCqU7UZ8oeGYsWYJPy0OHVTc76ZgkPVXGacFyMhB1YQKBgQDoun8GS+IwSPPCKnvg\npwXlNg4SwaSQqFxBxgCLPCi1DcefHggs21seN7UKIU2g7rI9qtE+2sFjx65WHfLu\nQ9k7D6qBaYLqp8a5G+h6OwfMbdoiEBXGO6uj3zzC9RaaFnFKJXGwPGmxj93VDxqD\nwZYtd9RsGTul3T8f6SWUhVLYDwKBgQDR51Hp6Fr1aR6GNyNsgaSMrPWXLAKDX1kW\n0gJf9b3QJDVMZ2IUJY2yI+ANYSrosl/z+IIcTpGKDFsu3fiepoJYXBulUziGfyMe\nJuTHHmSZ6NzlSF5+ikNr+JIU9DraucFAufUUZ2EHFASYfymhmZrz7GshQa+TdcYF\nBaY0KLGjiwKBgQCD4mPnmoskl8LjTpYC2kPh/7777BapD2+wqldWkBH1gEiWqDsz\nyF8bgbArfHH0eDAz+CA6AVfEhr0J+lDXq0VoTdlvGu7gsZFRBzq4QcmKlNofoYPl\nYZ1D8VcKmD9ltMAgp5WErMesov2wJq386nCDdbvAQpBlzzpdiA8O2nr3CwKBgQCi\nuDVbJEJnHw+HsnTmp/XdDDPy9i4Yk7DnrPQkWcfE9FzRFxAEWDxpTcBh/uKZs7YI\n1qxer1XJ89w4D+Gtc512TCkuV75pHWQm+rW92sTYP5oGsLx4Uk0EBZWRYufBKeEz\ngTUJlT61kwIhCetHGGH0GGHQTTCEGvfgasm2g5l6EwKBgQCAIsxY9oYU+AESYFrk\nrQ+CHsPr18UAfcPkTjisjAsCGro+vXKzwOqYZTVxRYEIABZyQJJoKcRFUwW+e5Hm\nvE0DTNTPb0sfXkZ63Kf/ZECDPcHSD1hmIOe2CXbdAArobSEqzqC/rVLdNUTZalkd\n2xspEwbtzabhxQGNNYfMo8sZfw==\n-----END PRIVATE KEY-----\n",
+//   "client_email": "firebase-adminsdk-fgrrr@socialape-4e17c.iam.gserviceaccount.com",
+//   "client_id": "118420894873735894282",
+//   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+//   "token_uri": "https://oauth2.googleapis.com/token",
+//   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+//   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fgrrr%40socialape-4e17c.iam.gserviceaccount.com"
+// }
 
-  /*
+
 
 module.exports = {
   apiKey: "AIzaSyBIKffOIRqs-CrOs8TP2iruDVRaTzughmY",
@@ -23,4 +23,4 @@ module.exports = {
   appId: "1:1092512616884:web:1df6c451777c50b63022e1",
   measurementId: "G-TV94T73RE6"
 };
-*/
+
