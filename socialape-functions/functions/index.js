57cb5aceeec31f53af75149d2dda101a8625de9a
colinosu <osullivancj@gmail.com>
2020-05-20 19:00:53 -0400

    Finished function code and created React boilerplate

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -121,13 +121,13 @@ exports.onUserImageChange = functions.firestore.document('/users/{userId}')
           data.forEach(doc => {
             batch.delete(db.doc(`/comments/${doc.id}`));
           })
-          return db.collection('likes').where('screamId', '==', screamId)
+          return db.collection('likes').where('screamId', '==', screamId).get();
         })
         .then(data => {
           data.forEach(doc => {
             batch.delete(db.doc(`/likes/${doc.id}`));
           })
-          return db.collection('notifications').where('screamId', '==', screamId)
+          return db.collection('notifications').where('screamId', '==', screamId).get();
         })
         .then(data => {
           data.forEach(doc => {
