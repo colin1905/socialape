a0e21466522b678d4a6da0e01a2c461a1dbbe06e
colinosu <osullivancj@gmail.com>
2020-05-24 18:39:09 -0400

    Login page redirects to home page for users already logged in. Signup page styling and error handling

--- socialape-client/src/pages/signup.js
+++ socialape-client/src/pages/signup.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import AppIcon from '../images/icon.png';
 import axios from 'axios';
 import { Link } from 'react-router-dom'
-
 // MUI stuff
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography'
@@ -16,31 +15,34 @@ const styles = (theme) => ({
   ...theme
 })
 
-
-export class login extends Component {
+export class signup extends Component {
   constructor(){
     super();
     this.state = {
       email: '',
       password: '',
+      confirmPassword: '',
+      handle: '',
       loading: false,
       errors: {}
     }
   }
-
   handleSubmit = (event) => {
     event.preventDefault();
     //send request to server, then show errors or redirect if successful
     this.setState({
       loading: true
     });
-    const userData = {
+    const newUserData = {
       email: this.state.email,
-      password: this.state.password
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword,
+      handle: this.state.handle
     }
-    axios.post('/login', userData)
+    axios.post('/signup', newUserData)
       .then(res => {
         console.log(res.data);
+        localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
         this.setState({
           loading: false
         });
@@ -71,27 +73,31 @@ export class login extends Component {
         <Grid item sm>
           <img src={AppIcon} alt="monkey" className={classes.image}/>
           <Typography variant="h1" className={classes.pageTitle}>
-            Login
+            Signup
           </Typography>
           <form noValidate onSubmit={this.handleSubmit}>
             <TextField id="email" name="email" type="email" label="Email" className={classes.textField}
              helperText = {errors.email} error={errors.email ? true : false} value={this.state.email} onChange={this.handleChange} fullWidth/>
             <TextField id="password" name="password" type="password" label="Password" className={classes.textField}
               helperText = {errors.password} error={errors.password ? true : false} value={this.state.password} onChange={this.handleChange} fullWidth/>
+            <TextField id="confirmPassword" name="confirmPassword" type="password" label="Confirm Password" className={classes.textField}
+              helperText = {errors.confirmPassword} error={errors.confirmPassword ? true : false} value={this.state.confirmPassword} onChange={this.handleChange} fullWidth/>
+            <TextField id="handle" name="handle" type="handle" label="Handle" className={classes.textField}
+              helperText = {errors.handle} error={errors.handle ? true : false} value={this.state.handle } onChange={this.handleChange} fullWidth/>
            {errors.general && (
              <Typography variant="body2" className={classes.customError}>
                {errors.general}
              </Typography>
            )}
             <Button type="submit" variant="contained" color="primary" className={classes.button} disabled={loading}>
-              LOGIN
+              Signup
               {loading && (
                 <CircularProgress size={30} className={classes.progress}/>
               )}
               </Button>
            
            <br />
-            <small>Don't have an account? <Link to="/signup">Sign up </Link></small>
+            <small>Already have an account? <Link to="/login">Login </Link></small>
           </form>
         </Grid>
         <Grid item sm/>
@@ -100,8 +106,8 @@ export class login extends Component {
   }
 }
 
-login.propTypes = {
+signup.propTypes = {
   classes: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(login)
+export default withStyles(styles)(signup)
