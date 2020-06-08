c1ab3e515da2ea23eb588f70263aab00655da76d
colinosu <osullivancj@gmail.com>
2020-06-08 17:21:02 -0400

    Redux setup

--- socialape-client/src/App.js
+++ socialape-client/src/App.js
@@ -5,6 +5,11 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import jwtDecode from 'jwt-decode';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+
 // Components
 import Navbar from './components/Navbar';
 import AuthRoute from './util/AuthRoute';
@@ -33,7 +38,7 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <div className="App">
+        <Provider store={store}>
           <Router>
             <Navbar />
             <div className="container">
@@ -44,7 +49,8 @@ class App extends Component {
               </Switch>
             </div>
           </Router>
-        </div>
+        </Provider>
+        
       </MuiThemeProvider>
     );
   }
--- socialape-client/src/pages/login.js
+++ socialape-client/src/pages/login.js
@@ -10,7 +10,10 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-import CircularProgress from '@material-ui/core/CircularProgress'
+import CircularProgress from '@material-ui/core/CircularProgress';
+// Redux stuff
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/userActions';
 
 const styles = (theme) => ({
   ...theme
@@ -23,36 +26,23 @@ export class login extends Component {
     this.state = {
       email: '',
       password: '',
-      loading: false,
       errors: {}
     }
   }
 
+  componentWillReceiveProps(nextProps){
+    if(nextProps.UI.errors){
+      this.setState({ errors: nextProps.UI.errors });
+    }
+  }
   handleSubmit = (event) => {
     event.preventDefault();
-    //send request to server, then show errors or redirect if successful
-    this.setState({
-      loading: true
-    });
+    
     const userData = {
       email: this.state.email,
       password: this.state.password
-    }
-    axios.post('/login', userData)
-      .then(res => {
-        console.log(res.data);
-        this.setState({
-          loading: false
-        });
-        this.props.history.push('/');
-      })
-      .catch(err => {
-        this.setState({
-          errors: err.response.data,
-          loading: false
-        }
-        )
-      })
+    } 
+    this.props.loginUser(userData, this.props.history)
 
   }  
 
@@ -62,8 +52,8 @@ export class login extends Component {
     });
   }
   render() {
-    const { classes } = this.props;    
-    const { errors, loading } = this.state;
+    const { classes, UI: { loading } } = this.props;    
+    const { errors } = this.state;
     
     return (
       <Grid container className={classes.form}>
@@ -101,7 +91,18 @@ export class login extends Component {
 }
 
 login.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  loginUser: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(login)
+const mapStateToProps = (state) => ({
+  user: state.user,
+  UI: state.UI
+});
+
+const mapActionsToProps = {
+  loginUser
+}
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(login))
