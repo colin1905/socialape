cb6831094b25e4ee4d3f8e6856ee34e7eb0f67b3
colinosu <osullivancj@gmail.com>
2020-06-14 19:50:52 -0400

    Like/unlike routes

--- socialape-client/src/components/EditDetails.js
+++ socialape-client/src/components/EditDetails.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
+import MyButton from '../util/MyButton';
 // Redux stuff
 import { connect } from 'react-redux';
 import { editUserDetails } from '../redux/actions/userActions';
@@ -11,8 +12,6 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
 // Icons
 import EditIcon from '@material-ui/icons/Edit';
 
@@ -21,7 +20,7 @@ const styles = (theme) => ({
   button: {
     float: 'right'
   }
-})
+});
 
 class EditDetails extends Component {
   state = {
@@ -30,27 +29,25 @@ class EditDetails extends Component {
     location: '',
     open: false
   };
-
   mapUserDetailsToState = (credentials) => {
     this.setState({
       bio: credentials.bio ? credentials.bio : '',
       website: credentials.website ? credentials.website : '',
-      location: credentials.location ? credentials.location : '',
-    })
-  }
-
+      location: credentials.location ? credentials.location : ''
+    });
+  };
   handleOpen = () => {
-    this.setState({ open: true })
+    this.setState({ open: true });
     this.mapUserDetailsToState(this.props.credentials);
-  }
+  };
   handleClose = () => {
     this.setState({ open: false });
-  }
-  componentDidMount(){
+  };
+  componentDidMount() {
     const { credentials } = this.props;
     this.mapUserDetailsToState(credentials);
   }
-  
+
   handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value
@@ -61,58 +58,61 @@ class EditDetails extends Component {
       bio: this.state.bio,
       website: this.state.website,
       location: this.state.location
-    }
+    };
     this.props.editUserDetails(userDetails);
     this.handleClose();
-  }
-
+  };
   render() {
     const { classes } = this.props;
     return (
       <Fragment>
-        <Tooltip title="Edit details" placement="top">
-          <IconButton onClick={this.handleOpen} className={classes.button}>
-            <EditIcon color="primary"/>
-          </IconButton>
-        </Tooltip>
+        <MyButton
+          tip="Edit Details"
+          onClick={this.handleOpen}
+          btnClassName={classes.button}
+        >
+          <EditIcon color="primary" />
+        </MyButton>
         <Dialog
-        open={this.state.open}
-        onClose={this.handleClose}
-        fullWidthmaxWidth="sm">
+          open={this.state.open}
+          onClose={this.handleClose}
+          fullWidth
+          maxWidth="sm"
+        >
           <DialogTitle>Edit your details</DialogTitle>
           <DialogContent>
             <form>
               <TextField
-              name="bio"
-              type="text"
-              label="Bio"
-              multiline
-              rows="3"
-              placeholder="A short bio about yourself"
-              className={classes.textField}
-              value={this.state.bio}
-              onChange={this.handleChange}
-              fullWidth
+                name="bio"
+                tpye="text"
+                label="Bio"
+                multiline
+                rows="3"
+                placeholder="A short bio about yourself"
+                className={classes.textField}
+                value={this.state.bio}
+                onChange={this.handleChange}
+                fullWidth
               />
               <TextField
-              name="website"
-              type="text"
-              label="Website"
-              placeholder="Your personal/professional website"
-              className={classes.textField}
-              value={this.state.website}
-              onChange={this.handleChange}
-              fullWidth
+                name="website"
+                tpye="text"
+                label="Website"
+                placeholder="Your personal/professinal website"
+                className={classes.textField}
+                value={this.state.website}
+                onChange={this.handleChange}
+                fullWidth
               />
               <TextField
-              name="location"
-              type="text"
-              label="Location"
-              placeholder="Where you livef"
-              className={classes.textField}
-              value={this.state.location}
-              onChange={this.handleChange}
-              fullWidth
+                name="location"
+                tpye="text"
+                label="Location"
+                placeholder="Where you live"
+                className={classes.textField}
+                value={this.state.location}
+                onChange={this.handleChange}
+                fullWidth
               />
             </form>
           </DialogContent>
@@ -126,16 +126,20 @@ class EditDetails extends Component {
           </DialogActions>
         </Dialog>
       </Fragment>
-    )
+    );
   }
 }
+
 EditDetails.propTypes = {
   editUserDetails: PropTypes.func.isRequired,
   classes: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = (state) => ({
   credentials: state.user.credentials
-})
+});
 
-export default connect(mapStateToProps, { editUserDetails })(withStyles(styles)(EditDetails))
+export default connect(
+  mapStateToProps,
+  { editUserDetails }
+)(withStyles(styles)(EditDetails));
